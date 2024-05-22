0e8eaacd0e983a9999f46301d872bd9546d78c58
Mattia Piazza <mattiapiazza@gmail.com>
2024-05-22 22:48:18 +0200

    added the change from number to letters in load_data

--- cbsd_hallucination_project/load_data.py
+++ cbsd_hallucination_project/load_data.py
@@ -2,11 +2,11 @@ import json
 from pathlib import Path
 import glob
 import os
+import numpy as np
 
 
 def data_extraction(data_path: Path) -> dict:
     json_files = glob.glob(os.path.join(data_path, "*.json"))
-    # print(f"\njson_files: {len(json_files)}\n")
 
     for json_file in json_files:
         # print(f"json_file # {json_files.index(json_file)}\n")
@@ -34,8 +34,48 @@ def data_extraction(data_path: Path) -> dict:
                         k not in my_data[key].keys()
                     ):  # Create a list for every k ('input', 'output') if k doesn't exist already in the dictionary
                         my_data[key][k] = []
-                    # print(
-                    #     f"key: \t{key}\nk: \t{k}\nv: \t{v}\nlen(my_data[key][k]): {len(my_data[key][k])}\n"
-                    # )
+
                     my_data[key][k].append(v)
+
+
     return my_data
+
+def boring_data_extraction(data_path:Path) -> json:
+    json_files = glob.glob(os.path.join(data_path, "*.json"))
+
+    for json_file in json_files:
+        # print(f"json_file # {json_files.index(json_file)}\n")
+
+        # Load the JSON content
+        with open(json_file, "r") as f:
+            data = json.load(f)
+        
+        print(f"This is the {data} data\n")
+        
+        input("Enter to continue")
+
+
+
+def from_num_to_col(arr: np.ndarray) -> str:
+    num_to_col = {
+        0: "X",  # Black
+        1: "B",  # Blue
+        2: "R",  # Red
+        3: "V",  # Green
+        4: "Y",  # Yellow
+        5: "G",  # Grey
+        6: "P",  # Pink
+        7: "O",  # Orrange
+        8: "C",  # Cyan
+        9: "M",  # Brown
+    }
+
+    col_array = []
+    n_row = 1
+
+    for row in arr:
+        row_str = str(n_row) + " -> " + "".join(num_to_col[i] for i in row) + "\n"
+        col_array.append(row_str)
+        n_row += 1
+
+    return "".join(col_array)
