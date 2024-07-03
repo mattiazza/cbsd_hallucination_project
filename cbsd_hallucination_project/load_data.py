177efcc423b38585b971703f6a5089b7abe35bc7
Mattia Piazza <mattiapiazza@gmail.com>
2024-07-03 15:41:37 +0200

    New load_data.py

--- cbsd_hallucination_project/load_data.py
+++ cbsd_hallucination_project/load_data.py
@@ -3,87 +3,108 @@ import json
 import os
 import glob
 from pathlib import Path
+from IPython.display import clear_output
 
 
-def data_extraction(data_path: Path) -> dict:
+def data_extraction(
+    data_path: Path, save_path: Path, llm: str, lim_print: int = 3
+) -> dict:
     """
     Extract file '.json' to a dictionary from the its path
     """
+    try:
+        with open(save_path / f"my_data_{llm}.json", "r") as f:
+            my_data = json.load(f)
 
-    json_files = glob.glob(os.path.join(data_path, "*.json"))
+        all_json_files = glob.glob(os.path.join(data_path, "*.json"))
+
+        for n, val in enumerate(my_data["test"]):
+            if llm not in val.keys():
+                json_files = all_json_files[
+                    n:
+                ]  # Select only the files we didn't already extract
+
+        json_files = all_json_files[n + 1 :]
+
+    except:
+        # Create my dictionary
+        my_data = {"train": [], "test": []}
+
+        # Taking all the path of our .json files
+        all_json_files = glob.glob(os.path.join(data_path, "*.json"))
+        json_files = all_json_files
 
     for json_file in json_files:
-        # print(f"json_file # {json_files.index(json_file)}\n")
+        print(
+            f"json_file # {all_json_files.index(json_file)+1}/{len(all_json_files)}\n"
+        )
 
         # Load the JSON content
         with open(json_file, "r") as f:
             data = json.load(f)
 
-        # Create my dictionary
-        if json_file == json_files[0]:
-            my_data = {}
+        dict_list = []
 
-        # Fill my_data dictionary
-        for (
-            key,
-            value,
-        ) in data.items():  # key: ['train', 'test'] value: list of dictionaries
-            if (
-                key not in my_data.keys()
-            ):  # Create a dictionary for every key ('train', 'test') if the key doesn't exist already in my_data
-                my_data[key] = {}
-            for val in value:
-                for k, v in val.items():  # k: ['input', 'output'], v: our target array
-                    if (
-                        k not in my_data[key].keys()
-                    ):  # Create a list for every k ('input', 'output') if k doesn't exist already in the dictionary
-                        my_data[key][k] = []
+        for n, dic in enumerate(data["train"]):
+            dic = number_to_colour(
+                dic
+            )  # Changing from numbers to letters our dictionary
+            dict_list.append(dic)  # Creating the list of dictionary
+            if n < lim_print:  # We decide how many training example we want to show
+                print(f"input_{n+1}:\n{dic['input']}\noutput_{n+1}:\n{dic['output']}\n")
 
-                    my_data[key][k].append(v)
+        my_data["train"].append(dict_list)  # Adding the result to our main dataset
+
+        # Adding the 'test' part
+        my_data["test"].append(number_to_colour(data["test"][0]))
+
+        print(f"Here's the test:\n{my_data['test'][-1]['input']}")
+
+        llm_says = input(
+            "Insert here LLM's output: "  # Saving the response of the LLM
+        )
+
+        my_data["test"][-1][llm] = llm_says
+
+        with open(save_path / f"my_data_{llm}.json", "w") as f:
+            json.dump(my_data, f)
+
+        clear_output(wait=True)  # Clear output each time
 
     return my_data
 
 
-def from_num_to_col(arr: np.ndarray) -> str:
+def number_to_letter(arr: np.ndarray) -> str:
     """
     Transform our input array in a list where each number correspond to a colour
     """
     num_to_col = {
-        0: "X",  # Black 
-        1: "B",  # Blue 
-        2: "R",  # Red 
+        0: "X",  # Black
+        1: "B",  # Blue
+        2: "R",  # Red
         3: "V",  # Green
-        4: "Y",  # Yellow 
-        5: "G",  # Grey 
-        6: "P",  # Pink 
-        7: "O",  # Orange 
-        8: "C",  # Cyan 
+        4: "Y",  # Yellow
+        5: "G",  # Grey
+        6: "P",  # Pink
+        7: "O",  # Orange
+        8: "C",  # Cyan
         9: "M",  # Brown
     }
 
     col_array = []
     n_row = 1
 
-    for row in arr:
+    for n_row, row in enumerate(arr):
         row_str = str(n_row) + " -> " + "".join(num_to_col[i] for i in row) + "\n"
         col_array.append(row_str)
-        n_row += 1
 
     return "".join(col_array)
 
 
 def number_to_colour(my_dict: dict) -> dict:
-    for key, value in my_dict.items():  # key: ['train', 'test']
-        for k, val in value.items():  # k: ['input', 'output']
-            temp = []
-
-            for v in val:  # We are taking one array at a time
-                array_colored = from_num_to_col(
-                    v
-                )  # Transform the array from number to colour (letter)
-                temp.append(array_colored)
-
-                
-            my_dict[key][k] = temp
+    for k, val in my_dict.items():  # k: ['input', 'output']; val: matrices
+        my_dict[k] = number_to_letter(
+            val
+        )  # Transform the array from number to colur (letter)
 
     return my_dict
